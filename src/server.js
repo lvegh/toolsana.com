e33313f8d90ef17a1fbabbdd8ea221cdef9858ef
lvegh87 <laszlo.vegh1987@gmail.com>
2025-07-17 13:56:17 +0200

    Testing

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { createUploadsDir } = require('./utils/fileSystem');
 
 // Load environment variables
 dotenv.config();
