9bd2c8685a43985fc26549a8386d863719f3129b
lvegh87 <laszlo.vegh1987@gmail.com>
2025-10-04 20:20:49 +0200

    png compressor compression improvements

--- src/services/pngOptimizer.js
+++ src/services/pngOptimizer.js
@@ -234,7 +234,7 @@ class PngOptimizer {
         let conservative = await imagemin.buffer(currentBuffer, {
           plugins: [
             imageminPngquant({
-              quality: [0.50, 0.70],
+              quality: [0.49, 0.69],
               speed: 1,
               strip: true,
               dithering: 1.0
